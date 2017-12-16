c0b314a4bcf7a8463b81f37ac2c5cbddffcc3b62
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-12-16 15:10:24 -0600

    Initial Commit

---
+++ EmojiScene/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiScene
+//
+//  Created by Dumitru Gutu on 12/16/17.
+//  Copyright © 2017 Dumitru Gutu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
